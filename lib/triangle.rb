ee9e661a198b98f9e14b4ded15b668f3179ffa01
Huy Do <huyddo40@gmail.com>
2019-07-02 18:13:31 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  attr_accessor :size1, :size2, :size3
+
+  def initialize(size1,size2,size3)
+    @size1 = size1
+    @size2 = size2
+    @size3 = size3
+  end
+
+  def kind
+    if size1==size2 && size2==size3
+      :equilateral
+    elsif size1==size2 || size2==size3 || size1 == size3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  class TriangleError < StandardError
+      # triangle error code
+  end
 end
