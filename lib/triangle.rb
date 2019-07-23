35defd806f2ef37973806c6fcfc9e7c5b1b62c9a
Huy Do <huyddo40@gmail.com>
2019-07-23 01:05:13 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,35 +8,35 @@ class Triangle
   end
 
   def kind
-    if (size1 <= 0 || size2 <= 0 || size3 <= 0) || !(size1 + size2 > size3 && size2 + size3 > size1 && size1 + size3 > size2)
-      # begin
-        raise TriangleError
-      # rescue TriangleError => error
-      #   puts error.message
-      # end
-    else
+      valid_triagle
       if size1==size2 && size2==size3
         :equilateral
       elsif size1==size2 || size2==size3 || size1 == size3
         :isosceles
-      elsif size1 + size2 > size3 && size2 + size3 > size1 && size1 + size3 > size2
+      # elsif size1 + size2 > size3 && size2 + size3 > size1 && size1 + size3 > size2
+     else
         :scalene
-      else
-        # begin
-          raise TriangleError
+      # else
+      #   # begin
+      #     raise TriangleError
         # rescue TriangleError => error
         #   puts error.message
         # end
       end
     end
-  end
 
-  class TriangleError < StandardError
-    # def message
-    #   "Sizes of triagle are need to be greater than 0 or
-    #   The sum of the lengths of any two sides of a triangle
-    #   always exceeds the length of the third side."
-    # end
-  end
+    def valid_triagle
+      if (size1 <= 0 || size2 <= 0 || size3 <= 0) || !(size1 + size2 > size3 && size2 + size3 > size1 && size1 + size3 > size2)
+          raise TriangleError
+      end
+    end
+
+    class TriangleError < StandardError
+      # def message
+      #   "Sizes of triagle are need to be greater than 0 or
+      #   The sum of the lengths of any two sides of a triangle
+      #   always exceeds the length of the third side."
+      # end
+    end
 
 end
